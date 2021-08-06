619cf1fc4677d9d93c182c11dd0f01fe36558334
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-08-05 22:53:12 -0500

    Completed Classes Exercise 1

---
+++ Automobiles/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automobiles
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+        public Car(string makeInput, string modelInput, int yearInput)
+        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = yearInput;
+        }
+        
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Automobiles/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Automobiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var caddy = new Car();
+            caddy.Make = "Cadillac";
+            caddy.Model = "El Dorado";
+            caddy.Year = 2000;
+
+            var prelude = new Car()
+            {
+                Make = "Honda",
+                Model = "Prelude",
+                Year = 2001
+            };
+
+            var myCar = new Car("Toyota", "camry", 2012);
+
+            var carList = new List<Car>() { caddy, prelude, myCar };
+            
+            foreach ( var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}, {vehicle.Model}, {vehicle.Year}");
+
+            }
+            
+        }
+    }
+}
